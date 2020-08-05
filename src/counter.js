dbd46e4cb1ce543f42e8b15bfc897b25cd46b3f2
Farhad <faradidev@gmail.com>
2020-08-05 16:17:57 +0430

    added counters

---
+++ src/counter.js
@@ -0,0 +1,18 @@
+const template = document.querySelector("#counter");
+
+export default function Counter(step) {
+	this.totalSteps = 0;
+
+	const counter = template.content.cloneNode(true);
+	counter.querySelector(".counter-step").innerText = step;
+	const totalStepsEl = counter.querySelector(".counter-total");
+	totalStepsEl.innerText = this.totalSteps;
+
+	const stepper = counter.querySelector(".stepper");
+	stepper.addEventListener("click", e => {
+		e.preventDefault();
+		totalStepsEl.innerText = this.totalSteps += step;
+	});
+
+	return counter;
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import "./styles/index.css";
+import Counter from './counter';
 
-const counterForm = document.querySelector("#counter-form");
-const counterInput = document.querySelector("#counter-input");
-const counterBtn = document.querySelector("#counter-btn");
+const counters = document.querySelector("#counters");
 
-const onAddCounter = e => {
-	e.preventDefault();
-	const step = Number(counterInput.value);
+if (document.createElement("template").content) {
+	const counterForm = document.querySelector("#counter-form");
+	const counterInput = document.querySelector("#counter-input");
+	const counterBtn = document.querySelector("#counter-btn");
+
+	const onAddCounter = e => {
+		e.preventDefault();
+		const step = Number(counterInput.value);
+
+		counters.appendChild(new Counter(step))
+		counterInput.value = null;
+	}
+
+	counterForm.addEventListener("submit", onAddCounter);
+	counterBtn.addEventListener("click", onAddCounter);
 }
-
-counterForm.addEventListener("submit", onAddCounter);
-counterBtn.addEventListener("click", onAddCounter);
+else {
+	counters.innerHTML = `
+	<div class="col browser-not-support-wrapper">
+		<div class='card'>
+			<div class='card-body'>
+				<p>Sorry, your browser does not supports html templates. try the latest version of major browsers</p>
+			</div>
+		</div>
+	</div>
+	`;
+}
