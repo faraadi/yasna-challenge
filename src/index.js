605066a62bb641c7a4aa5d6d180d09da3f4b49f0
Farhad <faradidev@gmail.com>
2020-08-05 13:38:41 +0430

    counter form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-import './styles/index.css';
+import "./styles/index.css";
+
+const counterForm = document.querySelector("#counter-form");
+const counterInput = document.querySelector("#counter-input");
+const counterBtn = document.querySelector("#counter-btn");
+
+const onAddCounter = e => {
+	e.preventDefault();
+	const step = Number(counterInput.value);
+}
+
+counterForm.addEventListener("submit", onAddCounter);
+counterBtn.addEventListener("click", onAddCounter);
